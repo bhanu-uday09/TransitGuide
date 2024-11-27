935b4adb3a24e41e2aa768efff3070c2ab87cab4
Bhanu Uday <140607545+bhanu-uday09@users.noreply.github.com>
2024-11-27 20:06:46 +0530

    Pre-Final2

--- fetch-functions.py
+++ fetch-functions.py
@@ -3,6 +3,70 @@ import json
 from datetime import datetime
 from config import get_indigo_db_connection,get_airindia_db_connection,get_spicejet_db_connection
 
+# Database connection details
+DB_NAME = "TransitGlobal"
+DB_USER = "postgres"
+DB_PASS = "0000"
+DB_HOST = "localhost"
+DB_PORT = "5432"
+
+# Function to get database connection
+def get_db_connection():
+    return psycopg2.connect(
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASS,
+        host=DB_HOST,
+        port=DB_PORT
+    )
+
+# Load the CSV data into a DataFrame
+airport_df = pd.read_csv('assets/airport_data.csv', usecols=['city', 'airport_code'])
+
+def get_closest_city(user_city):
+    """
+    Find the closest matching city to the user's input using fuzzy matching.
+    """
+    try:
+        # List of available city names
+        city_list = airport_df['city'].tolist()
+
+        # Perform fuzzy matching
+        closest_match, score, _ = process.extractOne(
+            user_city.strip().title(), city_list, scorer=fuzz.ratio
+        )
+
+        # Debugging: Print matching details
+        print(f"Input: {user_city}, Closest Match: {closest_match}, Score: {score}")
+
+        # Threshold to ensure it's a valid match
+        if score >= 50:  # 70 is a lenient similarity threshold
+            return closest_match
+        else:
+            return None
+    except Exception as e:
+        print(f"Error finding closest city: {e}")
+        return None
+
+
+def get_airport_code(user_city):
+    """
+    Get the airport code for the given city.
+    """
+    try:
+        # Find the closest matching city
+        closest_city = get_closest_city(user_city)
+
+        if closest_city:
+            # Filter the DataFrame to find the airport code for the closest city
+            result = airport_df[airport_df['city'] == closest_city]['airport_code']
+            return result.iloc[0] if not result.empty else None
+        else:
+            return None
+    except Exception as e:
+        print(f"Error fetching airport code for {user_city}: {e}")
+        return None
+    
 def fetch_indigo_flights(source_airport_code, destination_airport_code, journey_date):
     conn = http.client.HTTPSConnection("booking-com15.p.rapidapi.com")
     
